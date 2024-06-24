10125ae896eee9a72e99489912e7d3bb66b737ee
Palveet Kaur Saluja <52796065+Palveet@users.noreply.github.com>
2024-06-24 23:30:35 +0530

    if statement

--- src/Main.kt
+++ src/Main.kt
@@ -1,20 +1,20 @@
 //TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 var name:String = "Palveet" //top level variable + non null string
-var lastName:String?=null //nullable string
-var greeting = null //nothing type, non inferable
+//var lastName:String?=null //nullable string
+var greeting: String? = null //nothing type, non inferable
 fun main() {
-    val lang = "Kotlin" //inmmutable variable
-        //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-    // to see how IntelliJ IDEA suggests fixing it.
-    name = "Simran"
-    println("Hello, " + lang + "!")
-    println("Hello, " + name + "!")
-    println(lastName)
-
-    for (i in 1..5) {
-        //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-        // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-        println("i = $i")
+//    val lang = "Kotlin" //inmmutable variable
+//    name = "Simran"
+//    println("Hello, " + lang + "!")
+//    println("Hello, " + name + "!")
+//    println(lastName)
+//    greeting = "Hello"
+    if(greeting != null){
+        println(greeting)
+    } else {
+        println("Hi")
     }
+    println(name)
+
 }
