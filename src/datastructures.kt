c7271f083c34df1a6ad7df32ea486bd86d05474b
Palveet Kaur Saluja <52796065+Palveet@users.noreply.github.com>
2024-06-27 23:34:51 +0530

    array

--- src/datastructures.kt
+++ src/datastructures.kt
@@ -1,6 +1,30 @@
 fun sayHi(greeting:String, itemToGreet: String) = println("$greeting $itemToGreet")
 
 fun main(){
-    sayHi("Hey","Palveet")
-    sayHi("Hello","Simran")
+    val interestingThings = arrayOf("Kotlin","Programming","Comic Books")
+
+//    Array function example
+//    println(interestingThings.size)
+//    println(interestingThings[0])
+//    println(interestingThings.get(1))
+
+//    for loop syntax
+//    for(interestingThing in interestingThings){
+//        println(interestingThing)
+//    }
+//    sayHi("Hey","Palveet")
+//    sayHi("Hello","Simran")
+
+//    for each loop
+//    interestingThings.forEach { interestingThing ->
+//        println(interestingThing)
+//    }
+
+//    for each index function -> for iterating and maintaining index data
+
+//    interestingThings.forEachIndexed{index, interestingThing ->
+//        println("$interestingThing is at index $index")
+//    }
+
+    
 }
